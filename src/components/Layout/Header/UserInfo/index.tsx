ecb070dfd2d4ed56d4a9022996c3faca6c2f22dc
MrRaccxxn <ayrtonskt@gmail.com>
2022-12-28 00:45:16 -0400

    import fixed

--- src/components/Layout/Header/UserInfo/index.tsx
+++ src/components/Layout/Header/UserInfo/index.tsx
@@ -1,9 +1,9 @@
 import { useContext, useEffect, useState } from "react"
 import { IWeb3AuthContext } from "../../../../@types/context/web3AuthContext"
-import { Web3AuthContext } from "../../../../contexts/web3AuthContext"
+import { web3AuthContext } from "../../../../contexts/web3AuthContext"
 
 export const UserInfo = () => {
-    const { getUserInfo } = useContext(Web3AuthContext) as IWeb3AuthContext
+    const { getUserInfo } = useContext(web3AuthContext) as IWeb3AuthContext
     const [user, setUser] = useState<any>(null);
     useEffect(() => {
         const loadInfo = async () => {
