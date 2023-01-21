9c9c0d26a22a8c19dcd8786d3b5a3272342fa8e9
MrRaccxxn <ayrtonskt@gmail.com>
2023-01-21 08:36:25 -0400

    Twitter Seo fix

--- pages/_seo.tsx
+++ pages/_seo.tsx
@@ -12,23 +12,14 @@ const Meta = () => {
             <meta property="og:site_name" content="Even3" />
             <meta property="og:url" content="https://www.even3.app/" />
 
-            <meta name="twitter:title" content="" />
+            <meta name="twitter:title" content="Even3" />
             <meta name="twitter:description" content='We are a platorm focused on develop pleasant experiences for event hosts and attendees, providing trackeable and secure events, All for free!' />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:url" content="https://www.even3.app/" />
-            <meta name="twitter:image" content="https://even3.s3.us-west-1.amazonaws.com/Image+SEO.png" />
+            <meta name="twitter:image" content="https://raccoon-s3.s3.eu-central-1.amazonaws.com/Image+SEO.png" />
+            <meta name="twitter:creator" content='MrRaccxxn' key="twhandle" />
 
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://www.even3.app/" />
-            <meta property="twitter:title" content="Even3 - Unforgettable events" />
-            <meta property="twitter:description" content="We are a platorm focused on develop pleasant experiences for event hosts and attendees, providing trackeable and secure events, All for free!" />
-            <meta property="twitter:image" content="https://even3.s3.us-west-1.amazonaws.com/Image+SEO.png" />
-
-            <link rel="stylesheet" href="" />
-            <meta property="og:image" content="https://even3.s3.us-west-1.amazonaws.com/Image+SEO.png" />
-            <meta name="twitter:image" content="https://even3.s3.us-west-1.amazonaws.com/Image+SEO.png" />
-            <link rel="canonical" href="" />
-            <script type="text/javascript" src="" ></script>
+            <meta property="og:image" content="https://raccoon-s3.s3.eu-central-1.amazonaws.com/Image+SEO.png" />
         </Head>
     )
 }
